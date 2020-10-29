82244779b6dc1d184fff98f3eaf72293934b21d0
frenauvh <florian.renaut@corp.ovh.com>
2020-10-29 15:36:57 +0100

    feat(dedicated): add billing autorenew terminate tracking
    Signed-off-by: frenauvh <florian.renaut@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/terminate/terminate.controller.js
+++ packages/manager/apps/dedicated/client/app/billing/autoRenew/actions/terminate/terminate.controller.js
@@ -1,3 +1,5 @@
+import kebabCase from 'lodash/kebabCase';
+
 export default class {
   /* @ngInject */
   constructor($translate, atInternet) {
@@ -5,15 +7,24 @@ export default class {
     this.atInternet = atInternet;
   }
 
-  terminate() {
+  $onInit() {
     this.atInternet.trackClick({
-      name: 'autorenew::terminate',
+      name: `autorenew::${kebabCase(this.serviceType)}::delete`,
       type: 'action',
       chapter1: 'dedicated',
       chapter2: 'account',
       chapter3: 'billing',
     });
+  }
 
+  terminate() {
+    this.atInternet.trackClick({
+      name: `autorenew::${kebabCase(this.serviceType)}::delete::confirm`,
+      type: 'action',
+      chapter1: 'dedicated',
+      chapter2: 'account',
+      chapter3: 'billing',
+    });
     this.isDeleting = true;
     return this.terminateService()
       .then(() => this.onSuccess())
